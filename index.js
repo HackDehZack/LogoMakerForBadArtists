bbd07ad34fbded692ba431a8610a4b9628f6a9b8
HackDehZack <Zackseriousemail@gmail.com>
2023-12-01 17:33:09 -0500

    Jest and index tweaks

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 const inquirer = require('inquirer');
-const fs = requrie('fs');
+const fs = require('fs');
 
 const logoGenerator = require('./lib/logoGenerator');
 
-logoGenerator.run();
+// Prompt for user input using Inquirer
+inquirer
+  .prompt([
+    // Define your prompt questions here
+  ])
+  .then((answers) => {
+    // Process user input and generate the logo using the logoGenerator module
+    const logo = logoGenerator.generateLogo(answers);
+
+    // Write the generated logo to a file
+    fs.writeFile('logo.txt', logo, (err) => {
+      if (err) {
+        console.error('An error occurred while writing the logo file:', err);
+        return;
+      }
+      console.log('Logo file generated successfully!');
+    });
+  })
+  .catch((error) => {
+    console.error('An error occurred while prompting for user input:', error);
+  });
